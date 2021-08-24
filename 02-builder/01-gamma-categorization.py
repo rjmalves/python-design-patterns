d8006f8afb57163ba4006184897192ada41b6f1e
Rogerio Alves <rogerioalves.ee@gmail.com>
2021-08-23 23:15:34 -0300

    Começando builder

---
+++ 02-builder/01-gamma-categorization.py
@@ -0,0 +1,16 @@
+# Todos os padrões de design são divididos em três categorias
+# Isso é chamado de Gamma Categorization (Erich Gamma - GoF)
+
+# Creational Patterns
+# - Lida com a criação (construção) dos objetos
+# - Explícitos (construtor) vs. implícitos (dependency injection, reflection, etc.)
+# - Wholesale (único comando) vs. piecewise (passo a passo - objetos complexos)
+
+# Structural Patterns
+# - Preocupação com a estrutura do programa (membros das classes)
+# - Muitos padrões são wrappers que remetem à interface da classe interior
+# - Estressam a importância de um bom design de API
+
+# Behavioral Patterns
+# - São todos diferentes, não tem padrão.
+# - Tratam de todos os temas, mas são únicos.
